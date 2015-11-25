1d92816a2c700eb1afccdbe45d117a7d44aa6673
Byron Mo <byronmo@gmail.com>
2015-11-25 14:59:15 -0800

    Finish ActiveRecordLite

--- w3d5/active_record_lite/lib/03_associatable.rb
+++ w3d5/active_record_lite/lib/03_associatable.rb
@@ -20,6 +20,8 @@ class AssocOptions
 end
 
 class BelongsToOptions < AssocOptions
+  attr_reader :primary_key
+
   def initialize(name, options = {})
     @foreign_key = options[:foreign_key].nil? ? "#{name}_id".to_sym : options[:foreign_key]
     @primary_key = options[:primary_key].nil? ? :id : options[:primary_key]
@@ -29,7 +31,8 @@ end
 
 class HasManyOptions < AssocOptions
   def initialize(name, self_class_name, options = {})
-    @foreign_key = options[:foreign_key].nil? ? "#{self_class_name.downcase}_id".to_sym : options[:foreign_key]
+    # debugger
+    @foreign_key = options[:foreign_key].nil? ? "#{self_class_name.underscore}_id".to_sym : options[:foreign_key]
     @primary_key = options[:primary_key].nil? ? :id : options[:primary_key]
     @class_name = options[:class_name].nil? ? name.to_s.singularize.camelcase : options[:class_name].to_s.singularize.camelcase
   end
@@ -38,22 +41,29 @@ end
 module Associatable
   # Phase IIIb
   def belongs_to(name, options = {})
-    options = BelongsToOptions.new(name, options)
-    debugger
-    define_method "#{name}" do
-      foreign_key = options.foreign_key
+    self.assoc_options[name] = BelongsToOptions.new(name, options)
+    # options = BelongsToOptions.new(name, options)
+    define_method(name) do
+      options = self.class.assoc_options[name]
+      foreign_key = self.send(options.foreign_key)
       target_model_class = options.model_class
-
+      target_model_class.where(options.primary_key => foreign_key).first
     end
 
   end
 
   def has_many(name, options = {})
-    # ...
+    options = HasManyOptions.new(name, self.name, options)
+    define_method(name) do
+      primary_key = self.send(options.primary_key)
+      target_model_class = options.model_class
+      target_model_class.where(options.foreign_key => primary_key)
+    end
   end
 
   def assoc_options
     # Wait to implement this in Phase IVa. Modify `belongs_to`, too.
+    @assoc_options ||= {}
   end
 end
 
